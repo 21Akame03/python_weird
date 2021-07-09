c9d0ab87af14b781d41b686efa50a5e4b7c70bc0
21Akame03 <21akame03@gmail.com>
2021-07-09 23:46:33 +0400

    sniffer.py_httpchecks

--- net_tools/sniffer.py
+++ net_tools/sniffer.py
@@ -1,11 +1,23 @@
 import scapy.all as scapy
+import optparse
+from scapy.layers import http
 
-def sniff(interface) :
+def getArgs():
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--filter", dest="filter", help="Protocol Filter")
+    (options, arguments) = parser.parse_args()
+    return options
+
+def sniff(interface, Filter) :
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packets) :
-    print(packets)
+    if packets.haslayer(http.HTTPRequest) :
+        print("http heeeeee")
+        print(packets.show())
 
 
-sniff("wlp2s0")
+options = getArgs()
+sniff("eth0", options.filter)
+
 
